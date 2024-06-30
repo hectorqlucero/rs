6c24b66047546b00e65daf82ce6aacee02f9fefe
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2024-06-30 09:39:19 -0700

    migration 0016-proceso

---
+++ resources/migrations/016-proceso.up.sql
@@ -0,0 +1,6 @@
+create table proceso (
+  id int unsigned not null auto_increment primary key,
+  cliente_id int unsigned not null,
+  casa_id int unsigned not null,
+  creado timestamp default current_timestamp
+) engine=innodb default charset=utf8;
