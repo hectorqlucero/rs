334b3afa5982ab40cc362e55218f81e1f62b7171
Hector Lucero Quihuis <hectorqlucero@gmail.com>
2025-10-27 18:37:23 -0700

    Updated libraries

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :url "http://example.com/FIXME" ; Change me - optional
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.12.1"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [org.clojure/data.csv "1.1.0"]
                  [org.clojure/data.json "2.5.1"]
                  [org.slf4j/slf4j-simple "2.0.17"]
-                 [compojure "1.7.1"]
+                 [compojure "1.7.2"]
                  [hiccup "2.0.0"]
                  [buddy/buddy-hashers "2.0.167"]
                  [com.draines/postal "2.0.5"]
-                 [cheshire "6.0.0"]
+                 [cheshire "6.1.0"]
                  [clj-pdf "2.7.4"]
                  [ondrs/barcode "0.1.0"]
                  [pdfkit-clj "0.1.7"]
@@ -23,17 +23,17 @@
                  [org.clojure/java.jdbc "0.7.12"]
                  ;; Active JDBC drivers (MySQL, PostgreSQL, SQLite)
                  [mysql/mysql-connector-java "8.0.33"]
-                 [org.postgresql/postgresql "42.7.7"]
+                 [org.postgresql/postgresql "42.7.8"]
                  [org.xerial/sqlite-jdbc "3.50.3.0"]
                  ;; Optional JDBC drivers (uncomment if needed)
                  ;; [com.microsoft.sqlserver/mssql-jdbc "12.8.1.jre11"]   ; SQL Server
                  ;; [com.h2database/h2 "2.2.224"]                        ; H2
                  ;; [com.oracle.database.jdbc/ojdbc8 "21.11.0.0"]        ; Oracle (check licensing)
                  [ragtime "0.8.1"]
-                 [ring/ring-core "1.14.2"]
-                 [ring/ring-jetty-adapter "1.14.2"]
-                 [ring/ring-defaults "0.6.0"]
-                 [ring/ring-devel "1.14.2"]
+                 [ring/ring-core "1.15.3"]
+                 [ring/ring-jetty-adapter "1.15.3"]
+                 [ring/ring-defaults "0.7.0"]
+                 [ring/ring-devel "1.15.3"]
                  [ring/ring-codec "1.3.0"]]
        :main ^:skip-aot rs.core
        :aot [rs.core]
